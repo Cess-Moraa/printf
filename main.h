0b2ba09e8d3927c068c08b3da09c9f9c7a7fbf79
Cess-Moraa <cessmorah42@gmail.com>
2022-11-23 05:04:16 -0800

    Task printf

--- main.h
+++ main.h
@@ -1,7 +1,10 @@
-#ifndef MAINHMAIN
-#define HOLBERTONH
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_char(va_list c);
@@ -15,6 +18,18 @@ int print_octal(va_list o);
 int print_x(va_list x);
 int print_X(va_list X);
 int print_rot13(va_list R);
+int (*driver(char *format))(char *format, va_list);
+int _puts(char *string);
+int contadordigit(int number);
+int _abs(int number);
+int printpercent(char *format, va_list pa);
+int printhex(char *format, va_list);
+int printHEX(char *format, va_list);
+int printocta(char *format, va_list);
+int print_unsign(char *format, va_list);
+int printstr(char *format, va_list);
+int integer(int number);
+
 /**
  * struct code_format - Struct format
  *
